960390944a136a6d01b121ca4215b9fe6e970a59
ZeYn-Galaxy <shafwan066@gmail.com>
2023-10-12 09:14:06 +0800

    Fix 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Navbar from './assets/components/Navbar'
 import Sinopsis from './assets/components/Sinopsis'
 import Thumbnail from './assets/images/Thumbnail.jpeg'
